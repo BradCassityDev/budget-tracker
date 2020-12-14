7988b6af2370e6f64a939757bdc96fefa0e931e4
Brad Cassity <bradley.cassity@gmail.com>
2020-12-13 18:02:12 -0700

    Linked Manifest file to index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,23 @@
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-// const CACHE_NAME = 'budget-tracker-cache';
 const DATA_CACHE_NAME = 'budget-tracker-cache-v1';
 
 const FILES_TO_CACHE = [
-    './',
-    './index.html',
-    './js/index.js',
-    './js/idb.js',
-    './css/styles.css',
-    './manifest.json',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png'
+    '/',
+    '/index.html',
+    '/js/index.js',
+    '/js/idb.js',
+    '/manifest.json',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
   ];
 
   self.addEventListener('install', function (e) {
@@ -59,7 +58,6 @@ self.addEventListener('activate', function(e) {
             .then(cache => {
               return fetch(evt.request)
                 .then(response => {
-                  // If the response was good, clone it and store it in the cache.
                   if (response.status === 200) {
                     cache.put(evt.request.url, response.clone());
                   }
@@ -67,7 +65,6 @@ self.addEventListener('activate', function(e) {
                   return response;
                 })
                 .catch(err => {
-                  // Network request failed, try to get it from the cache.
                   return cache.match(evt.request);
                 });
             })
@@ -83,7 +80,6 @@ self.addEventListener('activate', function(e) {
             if (response) {
               return response;
             } else if (evt.request.headers.get('accept').includes('text/html')) {
-              // return the cached home page for all requests for html pages
               return caches.match('/');
             }
           });
